320d8a85856e58ca57d9b7c2ee372c7a061783ce
Satyam Zode <satyamzode@gmail.com>
2015-12-04 00:56:52 +0530

    /utils/database_accessor.go : 	* File to manage init db. 	* Add new method to init db

---
+++ utils/database_accessor.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"gopkg.in/mgo.v2"
+)
+
+//DatabaseAccessor : struct for binding mgo session, db url, db name and key.
+type DatabaseAccessor struct {
+	*mgo.Session
+	url  string
+	name string
+	key  int
+}
+
+/*NewDatabaseAccessor : Method to return DatabaseAccessor
+  instance and to initialize databse
+*/
+func NewDatabaseAccessor(url, name string, key int) *DatabaseAccessor {
+	session, _ := mgo.Dial(url)
+	session.DB(name).C("").EnsureIndex()
+	return &DatabaseAccessor{session, url, name, key}
+}
