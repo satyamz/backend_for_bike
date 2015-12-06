d2384b5acc8a01edbe4d6e2aeaa51be94c3f3e51
Satyam Zode <satyamzode@gmail.com>
2015-12-06 19:29:29 +0530

    utils/current_user_accessor.go : Modified * Changed key type to string

--- utils/current_user_accessor.go
+++ utils/current_user_accessor.go
@@ -1,16 +1,31 @@
 package utils
 
 import (
-	"gopkg.in/mgo.v2"
+	"github.com/gin-gonic/gin"
+	"github.com/satyamz/Bike/models"
+	// "gopkg.in/mgo.v2"
 )
 
 /*CurrentUserAccessor : Current user accessor data type for storing user key
  */
 type CurrentUserAccessor struct {
-	key int
+	key string
 }
 
 //NewCurrentUserAccessor : Function to return CurrentUserAccessor instance
-func NewCurrentUserAccessor(key int) *CurrentUserAccessor {
+func NewCurrentUserAccessor(key string) *CurrentUserAccessor {
 	return &CurrentUserAccessor{key}
+}
+
+//Get :function to return user instance associated with
+func (cua *CurrentUserAccessor) Get(c *gin.Context) *models.User {
+	if returnValue, _ := c.Get(cua.key); returnValue != "" {
+		return returnValue.(*models.User)
+	}
+	return nil
+}
+
+//Set : function to set the user and key together
+func (cua *CurrentUserAccessor) Set(c *gin.Context, user *models.User) {
+	c.Set(cua.key, user)
 }
