144814301a1799db78a6211f0dac99e20fe4865d
Satyam Zode <satyamzode@gmail.com>
2015-12-08 16:10:44 +0530

    web/routes.go : Modified

--- web/routes.go
+++ web/routes.go
@@ -2,21 +2,23 @@ package web
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/satyamz/Bike/controllers"
 	"github.com/satyamz/Bike/utils"
 )
 
 //Server : Struct for binding
-type Server struct {
-	*gin.Engine
-}
 
 //NewServer : function to instantiate Server
-func NewServer(dba utils.DatabaseAccessor, ca utils.CurrentUserAccessor) *Server {
-	router := Server{gin.Default()}
+func NewServer(dba utils.DatabaseAccessor, cua utils.CurrentUserAccessor) *gin.Engine {
+	router := gin.Default()
 	router.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"status": "Ok",
 		})
 	})
-	return &router
+
+	accountController := controllers.NewAccountController(dba, cua)
+	accountController.Register(router)
+
+	return router
 }
