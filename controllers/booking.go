875ad75f74f883243847cc5d32b8e42b040479b6
Satyam Zode <satyamzode@gmail.com>
2016-01-09 13:29:39 +0530

    Adding booking controllers. Status : On demand

---
+++ controllers/booking.go
@@ -0,0 +1,33 @@
+package controllers
+
+/*
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/satyamz/Bike/models"
+	"github.com/satyamz/Bike/utils"
+)
+
+//BookingController : Structure for booking controller
+type BookingController struct {
+	database    utils.DatabaseAccessor
+	currentUser utils.CurrentUserAccessor
+}
+
+//NewBookingController : Returns BookingController instance
+func NewBookingController(dba utils.DatabaseAccessor, cua utils.CurrentUserAccessor) *BookingController {
+	return &BookingController{
+		database:    dba,
+		currentUser: cua,
+	}
+}
+
+//Register : Function to register router.
+func (bc *BookingController) Register(router *gin.Engine) {
+	router.POST("/book/stop", RideStop)
+}
+
+//RideStop : To stop ride.
+func (bc *BookingController) RideStop(c *gin.Context) {
+
+}
+*/
---
+++ models/ride.go
@@ -0,0 +1,34 @@
+package models
+
+/*
+import (
+	// "gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+	"time"
+)
+
+//Ride : Structure to bind ride Information together
+type Ride struct {
+	UserID            bson.ObjectId `bson:"_id,omitempty"`
+	StartUserLocation []float64     `bson:"start_user_loc"`
+	EndUserLocation   []float64     `bson:"end_user_loc"`
+	RideStartTime     time.Time     `bson:"ride_start_time"`
+	RideEndTime       time.Time     `bson:"ride_start_time"`
+	RideStartReading  float64       `bson:"start_reading"`
+	RideEndReading    float64       `bson:"end_reading"`
+}
+
+//StopRide : function to return ride instance.
+func StopRide(userID bson.ObjectId, userLocation []float64, RideEndTime time.Time) {
+	return &Ride{
+	UserID:          userID,
+	EndUserLocation: userLocation,
+	RideEndTime:     RideEndTime,
+	}
+}
+
+//UserRequestStop : function to add user request to database.
+func (r *Ride) UserRequestStop() {
+
+}
+*/
